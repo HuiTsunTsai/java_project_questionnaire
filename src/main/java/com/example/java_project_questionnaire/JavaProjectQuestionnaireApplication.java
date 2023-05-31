8015d742433e4b122cdbb226f6b7f6773d5fc819
Hazel <twilight810108@gmail.com>
2023-05-31 16:52:01 +0800

    furst commit

---
+++ src/main/java/com/example/java_project_questionnaire/JavaProjectQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_project_questionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectQuestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_project_questionnaire/JavaProjectQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_project_questionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectQuestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
